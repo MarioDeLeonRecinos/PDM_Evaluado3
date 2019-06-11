0fdcfefa1e7ffef1b44bc359b1a46b46dc1739a3
MarioDeLeonRecinos <00353715@uca.edu.sv>
2019-06-10 19:21:32 -0600

    Retrofit implementado sin busqueda personalizada eso falta

---
+++ app/src/main/java/com/echavez/labo3evaluado/AppConstants.kt
@@ -0,0 +1,11 @@
+package com.echavez.labo3evaluado
+
+import com.echavez.labo3evaluado.Entities.Movie
+import com.echavez.labo3evaluado.Entities.MoviePreview
+
+object AppConstants {
+    val ombdApiKey = BuildConfig.OMBD_API_KEY
+    val ADD_TASK_REQUEST = 1
+    val emptymoviespreview = ArrayList<MoviePreview>()
+    val emptymovies = ArrayList<Movie>()
+}
