015ff17df6df6e23f99b09739b5fd73708e0bfa5
MarioDeLeonRecinos <00353715@uca.edu.sv>
2019-06-11 20:19:54 -0600

    api key movida porque no les funcionaba a unos del grupo

--- app/src/main/java/com/echavez/labo3evaluado/AppConstants.kt
+++ app/src/main/java/com/echavez/labo3evaluado/AppConstants.kt
@@ -4,7 +4,7 @@ import com.echavez.labo3evaluado.Entities.Movie
 import com.echavez.labo3evaluado.Entities.MoviePreview
 
 object AppConstants {
-    val ombdApiKey = BuildConfig.OMBD_API_KEY
+    val ombdApiKey = "a2419eae"
     val ADD_TASK_REQUEST = 1
     val emptymoviespreview = ArrayList<MoviePreview>()
     val emptymovies = ArrayList<Movie>()
