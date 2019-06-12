d977045ffe57ea84b319b6d610c997b98213b37b
RodrigoCruz1154 <00115417@uca.edu.sv>
2019-06-12 11:27:21 -0600

    Terminado

--- app/src/main/java/com/echavez/labo3evaluado/activities/NewMovieActivity.kt
+++ app/src/main/java/com/echavez/labo3evaluado/activities/NewMovieActivity.kt
@@ -1,7 +1,10 @@
 package com.echavez.labo3evaluado.activities
 
 import android.app.Activity
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
@@ -12,31 +15,33 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.echavez.labo3evaluado.AppConstants
 import com.echavez.labo3evaluado.Entities.MoviePreview
+import com.echavez.labo3evaluado.R
 import com.echavez.labo3evaluado.ViewModel.MovieViewModel
 import com.echavez.labo3evaluado.adapters.PreviewAdapter
 import kotlinx.android.synthetic.main.preview_add_movie.*
-import android.net.NetworkInfo
-import android.content.Context.CONNECTIVITY_SERVICE
-import androidx.core.content.ContextCompat.getSystemService
-import android.net.ConnectivityManager
-import android.content.Context
-import com.echavez.labo3evaluado.R
 
 
-class NewMovieActivity:AppCompatActivity() {
+class NewMovieActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.preview_add_movie)
 
         val MovieViewModel = ViewModelProviders.of(this).get(MovieViewModel::class.java)
-
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = cm.activeNetworkInfo
+        val isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting
         val layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
         val recyclerView = rv_preview
+
         val moviesPreviewAdapter = PreviewAdapter(movies = AppConstants.emptymoviespreview,
             clickListener = { movie: MoviePreview, checky: View ->
                 movie.selected = !movie.selected
-                Toast.makeText(this, if (movie.selected) "Selected ${movie.Title}" else "Unselected ${movie.Title}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    if (movie.selected) "Selected ${movie.Title}" else "Unselected ${movie.Title}",
+                    Toast.LENGTH_SHORT
+                ).show()
                 if (movie.selected) checky.visibility = View.VISIBLE else checky.visibility = View.GONE
             })
 
@@ -52,15 +57,19 @@ class NewMovieActivity:AppCompatActivity() {
 
         bt_search.setOnClickListener {
             val movieNameQuery = et_search.text.toString()
-            if (movieNameQuery.isNotEmpty() && movieNameQuery.isNotBlank()) {
-                MovieViewModel.fetchMovie(movieNameQuery)
-                MovieViewModel.getMovieListVM().observe(this, Observer { result ->
-                    moviesPreviewAdapter.changeDataSet(result)
-                })
+            if (isConnected) {
+                if (movieNameQuery.isNotEmpty() && movieNameQuery.isNotBlank()) {
+                    MovieViewModel.fetchMovie(movieNameQuery)
+                    MovieViewModel.getMovieListVM().observe(this, Observer { result ->
+                        moviesPreviewAdapter.changeDataSet(result)
+                    })
+                }
+            } else {
+                Toast.makeText(this, "No hay conexión", Toast.LENGTH_LONG).show()
             }
         }
 
-        bt_cancel.setOnClickListener {clearView(et_search, moviesPreviewAdapter)}
+        bt_cancel.setOnClickListener { clearView(et_search, moviesPreviewAdapter) }
 
         bt_add_preview.setOnClickListener {
             val thenownow = MovieViewModel.getMovieListVM().value
@@ -68,7 +77,7 @@ class NewMovieActivity:AppCompatActivity() {
 
             for (movie in selectedMovies) {
                 MovieViewModel.fetchMovieByTitle(movie.Title)
-                MovieViewModel.getMovieResult().observe(this, Observer {resultMovie ->
+                MovieViewModel.getMovieResult().observe(this, Observer { resultMovie ->
                     MovieViewModel.insert(resultMovie)
                 })
             }
@@ -78,17 +87,9 @@ class NewMovieActivity:AppCompatActivity() {
             finish()
         }
 
-        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        val activeNetwork = cm.activeNetworkInfo
-        val isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting
-        if (isConnected){
-            Toast.makeText(this,"Lol",Toast.LENGTH_SHORT)
-        }
-
     }
 
-    fun clearView(et: EditText, adapter: PreviewAdapter){
+    fun clearView(et: EditText, adapter: PreviewAdapter) {
         et.text.clear()
         adapter.changeDataSet(AppConstants.emptymoviespreview)
     }
